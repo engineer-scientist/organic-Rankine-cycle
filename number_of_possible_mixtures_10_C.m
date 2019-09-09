17502de876f013abe0e6fee2689444fba227cfa8
Sarthak Sharma <54902964+engineer-scientist@users.noreply.github.com>
2019-09-10 00:14:07 +0530

    Update number_of_possible_mixtures_10_C.m

--- number_of_possible_mixtures_10_C.m
+++ number_of_possible_mixtures_10_C.m
@@ -1,7 +1,5 @@
-% For step = 0.05 and 20 components: p = 41,685,448,298. [Run time = 24059.183 s (6 hours, 40 minutes, 59.183 seconds) on Intel Core i7-6700 CPU; 3.4 GHz, 3.4 GHz; 64 GB RAM.]
-% For step = 0.1 and 20 components: p = 16,648,863. [Run time = 17.192 s on Intel Core i7-6700 CPU; 3.4 GHz, 3.4 GHz; 64 GB RAM.]
-% For step = 0.05 and 10 components: p = 8,060,931. [Run time = 2.595 s on Intel Core i7-6700 CPU; 3.4 GHz, 3.4 GHz; 64 GB RAM.]
-% For step = 0.1 and 10 components: p = 76,290. [Run time = 0.077 s on Intel Core i7-6700 CPU; 3.4 GHz, 3.4 GHz; 64 GB RAM.]
+clc;
+clear;
 
 p = 0;
 step = 0.1;
@@ -28,3 +26,8 @@ for c1 = 0 : step : 1
 end
 disp (['For 10 components varied in steps of ', num2str(step), ', number of possible mixtures is ', num2str(p)])
 % disp (mass_composition)
+
+% For step = 0.05 and 20 components: p = 41,685,448,298. [Run time = 24059.183 s (6 hours, 40 minutes, 59.183 seconds) on Intel Core i7-6700 CPU; 3.4 GHz, 3.4 GHz; 64 GB RAM.]
+% For step = 0.1 and 20 components: p = 16,648,863. [Run time = 17.192 s on Intel Core i7-6700 CPU; 3.4 GHz, 3.4 GHz; 64 GB RAM.]
+% For step = 0.05 and 10 components: p = 8,060,931. [Run time = 2.595 s on Intel Core i7-6700 CPU; 3.4 GHz, 3.4 GHz; 64 GB RAM.]
+% For step = 0.1 and 10 components: p = 76,290. [Run time = 0.077 s on Intel Core i7-6700 CPU; 3.4 GHz, 3.4 GHz; 64 GB RAM.]
